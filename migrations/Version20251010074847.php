131a0c60661538bd92828645be9513cfa76cb89d
Einenlum <contact@einenlum.com>
2025-10-10 10:31:14 +0200

    migration

--- migrations/Version20251010074847.php
+++ migrations/Version20251010074847.php
@@ -21,7 +21,18 @@ final class Version20251010074847 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+
         )');
+        $this->addSql(<<<SQL
+            CREATE TABLE users (
+                    id INT AUTO_INCREMENT NOT NULL,
+                    email VARCHAR(180) NOT NULL,
+                    name VARCHAR(255) NOT NULL,
+                    password VARCHAR(255) NOT NULL,
+                    roles JSON NOT NULL,
+                    PRIMARY KEY(id)
+                )
+        SQL);
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON users (email)');
 
         $this->addSql('INSERT INTO users (email, name, password, roles) VALUES (?, ?, ?, ?)', [
